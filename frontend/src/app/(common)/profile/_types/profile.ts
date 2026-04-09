a33bf25381833500a592864fe348e36f3b46605b
byeolsi <dldntjr0516@gmail.com>
2026-04-09 13:12:41 +0900

    🐛 [USR] 마이 페이지 UI 디자인 수정

---
+++ frontend/src/app/(common)/profile/_types/profile.ts
@@ -0,0 +1,13 @@
+export interface Profile {
+  id: number;
+  loginId: string;
+  name: string;
+  email: string;
+  phone: string;
+  role: string;
+  birthDate: string;
+  gender: string;
+  nationality: string;
+  profileImage: string | null;
+  createdAt: string;
+}
